6a33b2ad6b6d18c254fda892612f34bfdb1dc5ac
Stéphane Van den Broeck <mail@stephanevdb.com>
2023-06-06 15:06:39 +0200

    added rickroll

--- modules/rickroll/payload.py
+++ modules/rickroll/payload.py
@@ -1,5 +1,7 @@
 import webbrowser
-
+import time
 url = "https://svdb.cc/r"
 
-webbrowser.open(url, new=0, autoraise=True)
+for i in range(0, 10):
+    webbrowser.open(url, new=0, autoraise=True)
+    time.sleep(10)
